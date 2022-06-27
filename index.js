12c413e0586423968d2f929957996e98b461fbc5
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-06-27 20:49:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+// @ts-nocheck
+/*
+
+This is how an item object should look like
+
+{
+  id: 1, // <- the item id matches the icon name in the assets/icons folder
+  name: "beetroot",
+  price: 0.35 // <- You can come up with your own prices
+}
+
+*/
+
+let state = {
+  storeItems: [
+    {
+      id: 1,
+      name: "beetroot",
+      price: 0.35,
+      icon: "001-beetrood.svg",
+    },
+    {
+      id: 2,
+      name: "carrot",
+      price: 0.35,
+      icon: "002-carrot.svg",
+    },
+    {
+      id: 3,
+      name: "apple",
+      price: 0.35,
+      icon: "003-apple.svg",
+    },
+    {
+      id: 4,
+      name: "apricot",
+      price: 0.35,
+      icon: "004-apricot.svg",
+    },
+    {
+      id: 5,
+      name: "avocado",
+      price: 0.35,
+      icon: "005-avocado.svg",
+    },
+    {
+      id: 6,
+      name: "bananas",
+      price: 0.35,
+      icon: "006-bananas.svg",
+    },
+    {
+      id: 7,
+      name: "bell-pepper",
+      price: 0.35,
+      icon: "007-bell-pepper.svg",
+    },
+    {
+      id: 8,
+      name: "berry",
+      price: 0.35,
+      icon: "008-berry.svg",
+    },
+    {
+      id: 9,
+      name: "blueberry",
+      price: 0.35,
+      icon: "009-blueberry.svg",
+    },
+    {
+      id: 10,
+      name: "eggplant",
+      price: 0.35,
+      icon: "010-eggplant.svg",
+    },
+  ],
+}
+
+
+  
+  
+
+  
